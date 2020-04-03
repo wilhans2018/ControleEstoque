e1d6ac6bf0808774f7f3f69a6641040868254ed1
Wilhans Souza <wilhans1984@hotmail.com>
2020-04-03 11:20:47 -0300

    Iniciando projeto

---
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -0,0 +1,104 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class CadastroController : Controller
+    {
+
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel() {Id=1, Nome="Livros", Ativo=true},
+            new GrupoProdutoModel() {Id=2, Nome="Mouses", Ativo=true},
+            new GrupoProdutoModel() {Id=3, Nome="Monitores", Ativo=false},
+        };
+       
+        [Authorize]
+        public ActionResult GrupoProduto()
+        {
+            return View(_listaGrupoProduto);
+        }
+
+
+        [Authorize]
+        public ActionResult RecuperarGrupoProduto()
+        {
+            return View(_listaGrupoProduto);
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult RecuperarGrupoProduto(int id)
+        {
+            return Json(_listaGrupoProduto.Find(x => x.Id == id));
+        }
+
+
+
+
+        [Authorize]
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult LocalProduto()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult UnidadeMedida()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Produto()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Pais()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Estado()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult PerfilUsuario()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleEstoque.Web/Models/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControleEstoque.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o seu Usúario")]
+        [Display(Name ="Usúario:")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Informe a sua senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha:")]
+        public string Senha { get; set; }
+
+
+
+        [Display(Name = "Lembrar me")]
+        public bool LembrarMe { get; set; }
+    }
+}
